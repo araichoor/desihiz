14f8a434d3bccf644ddf06bbc5e1cae321c12eb4
anand_raichoor <araichoor@lbl.gov>
2023-09-14 19:44:22 -0700

    settings for versioning

---
+++ py/desihizmerge/__init__.py
@@ -0,0 +1,15 @@
+#
+# See top-level LICENSE.rst file for Copyright information
+#
+# -*- coding: utf-8 -*-
+"""
+desihizmerge
+==============
+
+Tools for merging DESI hi-z catalogs.
+
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from ._version import __version__
